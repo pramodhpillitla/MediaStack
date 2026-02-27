5b49b645652f62330b261ff3caff97af0df7233d
Pramodh Pillitla <pramodhpillitla@gmail.com>
2026-02-27 23:59:01 +0530

    Add video upload routes with JWT verification

---
+++ src/routes/video.routes.js
@@ -0,0 +1,18 @@
+import { Router } from "express";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+import { upload } from "../middlewares/multer.middleware.js";
+import { uploadVideo } from "../controllers/video.controller.js";
+
+const router = Router();
+
+router.post(
+   "/",
+   verifyJWT,
+   upload.fields([
+      { name: "video", maxCount: 1 },
+      { name: "thumbnail", maxCount: 1 }
+   ]),
+   uploadVideo
+);
+
+export default router;
